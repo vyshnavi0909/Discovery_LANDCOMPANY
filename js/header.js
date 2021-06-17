f24b0d904f3d30ee29f5b120603897a1afa487a7
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-06-17 10:52:56 +0530

    complete about page

--- js/header.js
+++ js/header.js
@@ -1,13 +1,13 @@
-
 document.getElementById("header-part").innerHTML = `
+<header class="header" id="header-display">
     <div class="">
-        <a class="nav-a main-logo" href="./home.html">
-            <img src="../images/logo.svg" class="logo-img">
-            <div class="logo-text">
-                <span class="disc-text">Discovery</span>
-                <span class="tag-text">LAND COMPANY</span>
-            </div>
-        </a>
+            <a class="nav-a main-logo" href="./home.html">
+                <img src="../images/logo.svg" class="logo-img">
+                <div class="logo-text">
+                    <span class="disc-text">Discovery</span>
+                    <span class="tag-text">LAND COMPANY</span>
+                </div>
+            </a>
     </div>
     <div class="nav-bar">
         <ul class="nav-ul">
@@ -16,28 +16,55 @@ document.getElementById("header-part").innerHTML = `
             <li class="nav-item nav-sm"><a id="experiences" class="nav-a nav-list" href="./experiences.html">Experiences</a></li>
             <li class="nav-item nav-sm"><a id="gallery" class="nav-a nav-list" href="./gallery.html">Gallery</a></li>
             <li class="nav-item nav-sm"><a id="press" class="nav-a nav-list" href="./press.html">Press</a></li>
-            <li class="nav-item">
-                <div class="dropdown">
-                    <a href="javascript:void(0);" onclick="myFunction()" class="drpdwn-btn"><img
-                            class="menu-btn" src="../images/menu-bar.jpeg"></a>
-                    <div id="menu" class="menu-content">
-                        <div class="a-div">
-                            <a id="magazine" class="drpdwn-a" href="./magazine.html">Magazine</a>
-                        </div>
-                        <div class="a-div">
-                            <a id="company" class="drpdwn-a" href="./company.html">Company</a>
-                        </div>
-                        <div class="a-div">
-                            <a id="foundation" class="drpdwn-a" href="./foundation.html">Foundation</a>
-                        </div>
-                        <div class="a-div">
-                            <a id="career" class="drpdwn-a" href="./careers">Careers</a>
-                        </div>
-                        <div class="a-div">
-                            <a id="contact" class="drpdwn-a" href="./contact.html">Contact</a>
-                        </div>
-                    </div>
-                </div>
-            </li>
         </ul>
-    </div>`;
+    </div>
+    <div class="dropdown">
+        <a href="javascript:void(0);" onclick="dropdownFunc()" class="drpdwn-btn">
+            <img class="menu-btn" src="../images/menu-bar.jpeg">
+        </a>
+    </div>
+</header>
+<div id="menu" class="menu-content">
+<div class="menu-header">
+    <div>
+        <a class="nav-a main-logo" href="./home.html">
+            <img src="../images/logo.svg" class="logo-img">
+            <div class="logo-text">
+                <span class="disc-text">Discovery</span>
+                <span class="tag-text">LAND COMPANY</span>
+            </div>
+        </a>
+    </div>
+    <div class="dropdown">
+        <a href="javascript:void(0);" onclick="dropdownFunc()" class="drpdwn-btn">
+            <img class="menu-btn" src="../images/menu-bar.jpeg">
+        </a>
+    </div> 
+</div>
+<div class="menu-div">
+    <div class="a-div">
+            <a id="magazine" class="drpdwn-a" href="./magazine.html">Magazine</a>
+    </div>
+</div>
+<div class="menu-div">
+    <div class="a-div">
+        <a id="company" class="drpdwn-a" href="./company.html">Company</a>
+    </div>
+</div>
+<div class="menu-div">
+    <div class="a-div">
+        <a id="foundation" class="drpdwn-a" href="./foundation.html">Foundation</a>
+    </div>
+</div>
+<div class="menu-div">
+    <div class="a-div ">
+        <a id="career" class="drpdwn-a" href="./careers">Careers</a>
+    </div>
+</div>
+<div class="menu-div">
+    <div class="a-div">
+        <a id="contact" class="drpdwn-a" href="./contact.html">Contact</a>
+    </div>
+</div>
+
+</div>`;
--- js/home.js
+++ js/home.js
@@ -1,14 +1,10 @@
-function myFunction() {
+function dropdownFunc() {
     var drpdwn = document.getElementById("menu");
     if (drpdwn.style.display === "block") {
         drpdwn.style.display = "none";
     } else {
         drpdwn.style.display = "block";
     }
-    // document.getElementById("menu").classList.toggle("show");
 }
 
-function scrollingFunc() {
-    var elmt = document.getElementById("s3-images");
-    var leftScroll = elmt.scrollLeft;
-}
+
