1d579381ca52c2b008e18eb7d246d359a05a76dd
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-06-20 20:56:50 +0530

    ADD complete magazine page

--- js/scroller.js
+++ js/scroller.js
@@ -1,6 +1,4 @@
 
-
-
 var images = ["image-3.jpg","image-6.jpg","image-21.jpg","image-20.jpg","image-13.jpeg","image-25.jpg","image-17.jpg","image-7.jpg","image-10.jpg","image-14.jpg","image-4.jpg","image-16.jpg","image-18.jpg","image-8.jpg","image-24.jpg","image-12.jpg","image-5.jpg","image-23.jpg","image-9.jpg","image-22.jpg","image-15.jpeg","image-27.jpg","image-26.jpg","image-11.jpg","image-19.jpg"];
 
 function scrollingFunc(item){
